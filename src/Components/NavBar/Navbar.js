f1dcc293ad1021c3f22efd2c4c8a3d17baa23bc9
Matthew Storey <mattstorey@hotmail.co.uk>
2021-10-23 01:18:21 +0100

    created functioning navbar and added basic styling

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,15 +1,57 @@
 import './navbar.css'
 
-import {Link}from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const navbar = () => {
-    return (
-        <nav className="nav-bar">
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="/work">Work</Link>
-            <Link className="link" to="/contact">Contact</Link>
-        </nav>
-    )
+	return (
+		<nav className="nav-bar-links">
+			<div className="nav-links">
+				<NavLink
+					activeStyle={{ fontWeight: '300' }}
+					className='link'
+					to='/'
+					exact
+				>
+					Home
+				</NavLink>
+				<NavLink
+					activeStyle={{ fontWeight: '300' }}
+					className='link'
+					to='/work'
+					exact
+				>
+					Work
+				</NavLink>
+				<NavLink
+					activeStyle={{ fontWeight: '300' }}
+					className='link'
+					to='/contact'
+					exact
+				>
+					Contact
+				</NavLink>
+			</div>
+			<div className='language-links'>
+				<NavLink
+					activeStyle={{ fontWeight: '400' }}
+					className='link'
+					to='/'
+					exact
+				>
+					EN
+				</NavLink>
+				|
+				<NavLink
+					activeStyle={{ fontWeight: '400' }}
+					className='link'
+					to='/jp/'
+					exact
+				>
+					JP
+				</NavLink>
+			</div>
+		</nav>
+	)
 }
 
 export default navbar

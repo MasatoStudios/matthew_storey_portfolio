37ad19b96384622f65943b658dfae26549f2e6d9
Matthew Storey <mattstorey@hotmail.co.uk>
2021-10-23 01:18:53 +0100

    init home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import Navbar from "../Components/NavBar/Navbar"
+import Template from '../Components/Template/Template'
 
 const Home = () => {
     return (
         <>
-           <Navbar /> 
-           <h1>This is the homepage</h1>
+            <Template>
+                <h1>This is the home page</h1>
+            </Template>
         </>
     )
 }

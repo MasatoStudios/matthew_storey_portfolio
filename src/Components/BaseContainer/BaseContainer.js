66f4b1c4950ac27294d7e1af95e13fd4828c3b39
Matthew Storey <mattstorey@hotmail.co.uk>
2021-10-24 17:19:17 +0100

    added css links

--- src/Components/BaseContainer/BaseContainer.js
+++ src/Components/BaseContainer/BaseContainer.js
@@ -1,12 +1,13 @@
 import Container from '@mui/material/Container'
 import CssBaseLine from '@mui/material/CssBaseline'
 import './BaseContainer.css'
+import '../utils/variables.css';
 
 const BaseContainer = ({children}) => {
 	return (
 		<>
 			<CssBaseLine />
-			<Container maxWidth='xl' sx={{ bgcolor: '#cfe8fc' }}>
+			<Container maxWidth='xl' className="yellow-test">
                 {children}
             </Container>
 		</>

0e1b05337df5c68778a0070044ae7c13ae57e5fc
Matthew Storey <mattstorey@hotmail.co.uk>
2021-11-02 23:08:37 +0000

    created title component

---
+++ src/Components/Home/Title/Title.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+import './title.css'
+
+const Title = () => {
+
+	
+	return (
+		<div className='front-page-container'>
+			<div class='flex-container'>
+				<div class='title-container'>
+					<div class="text-wrapper">
+						<h1 className='name-title'>Matthew Storey</h1>
+						<h2 className='description-title'>Freelance Front-End Developer</h2>
+					</div>
+				</div>
+			</div>
+			<span className='scroll-down'>Scroll Down</span>
+		</div>
+	)
+}
+
+export default Title

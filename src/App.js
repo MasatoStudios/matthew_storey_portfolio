3666b5ef778776158705d301c230c0ccc76d8c3c
Matthew Storey <mattstorey@hotmail.co.uk>
2021-11-06 19:24:06 +0000

    updated routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@
 
 import Home from './pages/Home'
 import Work from './pages/Work'
+import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
 import Error from './pages/Error'
 
 import './App.css';
 import { Route, Switch } from 'react-router';
+import UniSite from './pages/UniSite'
 
 function App() {
   return (
     <main>
       <Switch>
         <Route path="/" component={Home} exact></Route>
-        <Route path="/work" component={Work}></Route>
-        <Route path="/contact" component={Contact}></Route>
+        <Route path="/work" exact component={Work}></Route>
+        <Route path="/work/portfolio" exact component={Portfolio}></Route>
+        <Route path="/work/uni-site" exact component={UniSite}></Route>
+        <Route path="/contact" exact component={Contact}></Route>
         <Route component={Error} />
       </Switch>
     </main>

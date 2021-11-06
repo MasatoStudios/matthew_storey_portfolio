40bbee640835118fa0815a4ff14c5c47c5f548d7
Matthew Storey <mattstorey@hotmail.co.uk>
2021-11-06 19:24:52 +0000

    Created Title Components

---
+++ src/Components/Contact/Title/Title.js
@@ -0,0 +1,11 @@
+import './Title.css'
+const Title = () => {
+    return (
+        <div className='title-wrapper'>
+           <h1 className='first-title'>Say hello</h1> 
+           <h3 className='second-title'>Connect with me</h3> 
+        </div>
+    )
+}
+
+export default Title

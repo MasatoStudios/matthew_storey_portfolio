a76675c188829c084fef19f00d87f688ca33d182
Matthew Storey <mattstorey@hotmail.co.uk>
2021-11-07 00:20:03 +0000

    updated styles

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,18 +6,32 @@ import '../Components/utils/variables.css'
 const Portfolio = () => {
 	return (
 		<Template>
-			<p>
-				Working on this project was the most freedom I’ve had in a project so
-				far i was the lead developer, designer and manager whilst being the
-				client at the same time. My vision for this was to create a minimalistic
-				site which showcases all my skills. The whole site uses ES6 standards
-				such as arrow functions and hooks with props. Overall I am really happy
-				with the final piece. However in the future I do plan to add a
-				playgrounds tab where I can add all experimental features I create.
-				<div className='image-container'>
-					<img src={Image} alt='Mockup of the site' className='case-image' />
-				</div>
-			</p>
+			<div className='image-container'>
+				<img src={Image} alt='Mockup of the site' className='case-image' />
+			</div>
+			<div class='text-container'>
+				<p className='showcase-text'>
+					Working on this project was the most freedom I’ve had in a project so
+					far. I was the lead developer, designer and manager whilst being the
+					client at the same time. My vision for this was to create a
+					minimalistic site which showcases all my skills. The whole site uses
+					ES6 standards such as arrow functions and hooks with props. Overall I
+					am really happy with the final piece. However, in the future I do plan
+					to add a playgrounds tab where I can add all experimental features I create.
+				</p>
+				<h3 className='technologies'>Technologies used:</h3>
+				<ul className='bullet-list'>
+					<li className='bullet-points'><span>React with hooks(ES6)</span></li>
+					<li className='bullet-points'><span>React-router-dom</span></li>
+					<li className='bullet-points'><span>Material UI</span></li>
+					<li className='bullet-points'><span>Font Awesome for React</span></li>
+				</ul>
+                <ul className='bullet-list'>
+                    <li className='bullet-points'><span>Figma (Prototyping)</span></li>
+                    <li className='bullet-points'><span>Adobe Photoshop (Mockups)</span></li>
+                    <li className='bullet-points'><span>Adobe Illustrator (Graphics)</span></li>
+                </ul>
+			</div>
 		</Template>
 	)
 }
--- src/pages/UniSite.js
+++ src/pages/UniSite.js
@@ -1,7 +1,58 @@
 import Template from '../Components/Template/Template'
+import Image from '../images/Mockup.png'
+
+import { Link } from 'react-router-dom'
 
 const UniSite = () => {
-	return <Template>This is the Uni site</Template>
+	return (
+		<Template>
+			<div className='image-container'>
+				<img src={Image} alt='Mockup of the site' className='case-image' />
+			</div>
+			<div class='text-container'>
+				<p className='showcase-text'>
+					Luxury Choice Getaway was a fictional holiday booking company who
+					needed re-branding and updating. In our 10 week semester, we were
+					tasked to create a full site consisting of an index page, view
+					holidays and Admin page. The wireframes and credits pages were created
+					for more additional tasks towards our assignment. Throughout this
+					project, we were not allowed to use any online generators or resorces
+					for the design. All was built from scratch. We also used vanilla PHP
+					for the View Holidays section which was challenging but an overall
+					enjoyable task. The site is still hosted, so feel free to check it out{' '}
+					<Link
+						to={{
+							pathname: 'http://unn-w20003752.newnumyspace.co.uk/index.html',
+						}}
+						target='_blank'
+						className='hover contact-link'
+					>
+						here:
+					</Link>
+				</p>
+				<h3 className='technologies'>Technologies used:</h3>
+				<ul className='bullet-list'>
+					<li className='bullet-points'>
+						<span>HTML and CSS</span>
+					</li>
+					<li className='bullet-points'>
+						<span>Javascript (animated hamburger menu)</span>
+					</li>
+					<li className='bullet-points'>
+						<span>PHP (Backend)</span>
+					</li>
+				</ul>
+				<ul className='bullet-list'>
+					<li className='bullet-points'>
+						<span>Adobe Photoshop (Mockups)</span>
+					</li>
+					<li className='bullet-points'>
+						<span>Adobe Illustrator (Graphics)</span>
+					</li>
+				</ul>
+			</div>
+		</Template>
+	)
 }
 
 export default UniSite
